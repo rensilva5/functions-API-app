0ea84b7fc8597f75e5db91a7c850594be6ad3484
rensilva5 <renzoasilva@gmail.com>
2022-09-01 13:37:00 -0400

    update updatetask function

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,7 +1,7 @@
 import dbConnect from "./dbConnect.js";
 
 export async function getTasks(req, res) {
-  //add by suer id to this
+  //add by user id to this
   const db = dbConnect();
   const collection = await db
     .collection("task")
@@ -17,10 +17,11 @@ export async function getTasks(req, res) {
 }
 
 export async function createTask(req, res) {
-  // will addee user id
+  // It will add a user id
   const newTask = req.body;
   if (!newTask || !newTask.task) {
-    res.status(400).send({ success: false, message: "invalid request" });
+    res.status(400)
+    .send({ success: false, message: "invalid request" });
     return;
   }
   const db = dbConnect();
@@ -33,10 +34,15 @@ export async function createTask(req, res) {
   getTasks(req, res); // send back the full list task...
 }
 
-export function updateTask(req, res) {
+export async function updateTask(req, res) {
   const taskUpdate = req.body;
   const { taskId } = req.params;
-  res.status(202).send("Task updated");
+  const db = dbConnect()
+  await db.collection('task').doc(taskID).update(taskUpdate)
+  .catch((err) => res.status(500).send(err));
+
+  res.status(202)
+  getTasks(req, res)
 }
 
 export function deleteTask(req, res) {

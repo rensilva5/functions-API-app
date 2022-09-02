74f08ab84fc238c0547cd216cf67644533d02a27
rensilva5 <renzoasilva@gmail.com>
2022-09-01 22:44:45 -0400

    Fixing and debbuging

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 import dbConnect from "./dbConnect.js";
-import { secrectKey } from "../credentials.js";
+import { secretKey } from "../credentials.js";
 
-export async function getTasks(req, res) {
+export async function getTasks (req, res) {
   const token = req.headers.authorization;
-  const user = jwt.verify(token, secrectKey);
+  const user = jwt.verify(token, secretKey);
   const db = dbConnect();
   const collection = await db
-    .collection("task")
+    .collection("tasks")
     .where("userId", "==", user.id)
     .get()
     .catch((err) => res.status(500).send(err));
-  const tasks = collection.docs.map((doc) => {
+  const tasks = collection.docs.map(doc => {
     // return {...doc.data(), id: doc.id}
     let task = doc.data();
     task.id = doc.id;
@@ -23,7 +23,7 @@ export async function getTasks(req, res) {
 export async function createTask(req, res) {
   const token = req.headers.authorization;
   let newTask = req.body;
-  const user = jwt.verify(token, secrectKey);
+  const user = jwt.verify(token, secretKey);
   if (!newTask || !newTask.task || !user) {
     res.status(400).send({ success: false, message: "invalid request" });
     return;
@@ -31,7 +31,7 @@ export async function createTask(req, res) {
   newTask.userId = user.id;
   const db = dbConnect();
   await db
-    .collection("task")
+    .collection("tasks")
     .add(newTask)
     .catch((err) => res.status(500).send(err));
   res.status(201);
@@ -44,7 +44,7 @@ export async function updateTask(req, res) {
   const { taskId } = req.params;
   const db = dbConnect();
   await db
-    .collection("task")
+    .collection("tasks")
     .doc(taskId)
     .update(taskUpdate)
     .catch((err) => res.status(500).send(err));
